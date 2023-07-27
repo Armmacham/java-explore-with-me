4a63a6a3e7818d4cb9df28e911989e9256166f43
Aleksandr <exodusong@gmail.com>
2023-07-27 13:38:58 +0300

    Дипломный проект. Этап 2. Версия 2.1

--- ewm-service/src/main/java/ru/practicum/ewmservice/category/controller/dto/AddCategoryRequestDto.java
+++ ewm-service/src/main/java/ru/practicum/ewmservice/category/controller/dto/AddCategoryRequestDto.java
@@ -2,6 +2,7 @@ package ru.practicum.ewmservice.category.controller.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
